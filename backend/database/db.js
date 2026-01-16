0328a9834649a2669eecfdef8289d1be2c10bb5b
guilherme <guibferrarezi@gmail.com>
2026-01-16 16:16:29 -0300

    conexao com db agora usa url

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,4 @@
+// db.js
 import pkg from "pg";
 import dotenv from "dotenv";
 
@@ -5,15 +6,18 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+// Cria o Pool de conexão
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
+// Evento de conexão
 pool.on("connect", () => {
   console.log("PostgreSQL conectado");
 });
 
+// Evento de erro
+pool.on("error", (err) => {
+  console.error("Erro no pool de conexão:", err);
+});
