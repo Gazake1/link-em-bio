3ccd2fbeef15f4e253bdbf753016502de71bd8d5
guilherme <guibferrarezi@gmail.com>
2026-04-29 15:01:38 -0300

    ieoeoeoeoeeo

---
+++ react/dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,90 @@
+export default {
+  "bootstrapScriptContent": "import(\"/saiba-mais/assets/index-CJsDinB3.js\")",
+  "clientReferenceDeps": {
+    "2e86ca7e1157": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "19cc3f2900f4": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "05e358f6455c": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "a8476ac92304": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "c2747888630f": {
+      "js": [
+        "/saiba-mais/assets/facade__virtual_vinext-rsc-entry-CwfOiNdc.js",
+        "/saiba-mais/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/saiba-mais/assets/index-CJsDinB3.js",
+        "/saiba-mais/assets/preload-helper-CjDaxxhm.js",
+        "/saiba-mais/assets/framework-BcnTjyNi.js",
+        "/saiba-mais/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/saiba-mais/assets/index-ClFcb3cA.css"
+      ]
+    }
+  }
+}
